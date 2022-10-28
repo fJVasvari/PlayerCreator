d2d7d0fd208d7b715f7d3dbfbd1dbd7bba63e84a
fanaczan <fanaczan@vasvari.org>
2022-10-28 08:16:16 +0200

    Spinner értékek hozzáadva

--- app/src/main/java/com/example/playercreator/MainActivity.java
+++ app/src/main/java/com/example/playercreator/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.playercreator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner playerClassSpinner, playerGenderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerClassSpinner = findViewById(R.id.classSpinner);
+        playerGenderSpinner = findViewById(R.id.genderSpinner);
+
+        String[] pClasses = new String[]{"Barbarian","Mage"};
+        final ArrayAdapter<String> pClassAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, pClasses);
+        playerClassSpinner.setAdapter(pClassAdapter);
+
+        String[] pGenders = new String[]{"Male","Female"};
+        final ArrayAdapter<String> pGenderAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, pGenders);
+        playerGenderSpinner.setAdapter(pGenderAdapter);
     }
 }
