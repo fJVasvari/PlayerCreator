e856862b8e9b0da150aa71d3f1b0e44b062188a7
fanaczan <fanaczan@vasvari.org>
2022-11-11 08:40:36 +0100

    Nem jó a feltöltés, de haladtunk a buliba

--- app/src/main/java/com/example/playercreator/DataBase.java
+++ app/src/main/java/com/example/playercreator/DataBase.java
@@ -1,5 +1,7 @@
 package com.example.playercreator;
 
+import android.widget.Toast;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
--- app/src/main/java/com/example/playercreator/MainActivity.java
+++ app/src/main/java/com/example/playercreator/MainActivity.java
@@ -6,20 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner playerClassSpinner, playerGenderSpinner;
+    Spinner playersSpinner, playerClassSpinner, playerGenderSpinner;
     Button createBtn, updateBtn, deleteBtn, refreshBtn;
 
+    EditText nameText;
+
+    DataBase dataBase = new DataBase();
+    ArrayList<Player> playerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPlayersFromDB();
 
+
+
+        playersSpinner = findViewById(R.id.playerSpinner);
         playerClassSpinner = findViewById(R.id.classSpinner);
         playerGenderSpinner = findViewById(R.id.genderSpinner);
+
+        nameText = findViewById(R.id.nameEditText);
 
         createBtn = findViewById(R.id.create_btn);
         updateBtn = findViewById(R.id.update_btn);
@@ -34,10 +48,16 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<String> pGenderAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, pGenders);
         playerGenderSpinner.setAdapter(pGenderAdapter);
 
+        String[] pNames = new String[]{"pista"};
+        final ArrayAdapter<String> playerNameAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, pNames);
+        playersSpinner.setAdapter(playerNameAdapter);
+
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Player player = new Player(nameText.getText().toString(),playerClassSpinner.getSelectedItem().toString(),playerGenderSpinner.getSelectedItem().toString());
+                dataBase.playerUpload(player);
+                Toast.makeText(getApplicationContext(),"szöveg",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -61,5 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void getPlayersFromDB(){
+        playerList = dataBase.getAllPlayer();
     }
 }
