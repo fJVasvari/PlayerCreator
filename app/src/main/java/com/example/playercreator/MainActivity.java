e3000f7055afce1775095fdbbb907c58893335c1
fanaczan <fanaczan@vasvari.org>
2022-12-14 08:29:04 +0100

    getJson elkezdve

--- app/src/main/java/com/example/playercreator/MainActivity.java
+++ app/src/main/java/com/example/playercreator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.playercreator;
 import androidx.appcompat.app.AppCompatActivity;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -16,6 +17,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -88,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         textviewDamage.setText(String.valueOf(playerDamage));
         textviewDefense.setText(String.valueOf(playerDefense));
         textviewStatPoints.setText(String.valueOf(statPoint));
+        getJSON("http://10.0.14.100/playercreator/getdata.php");
 
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -212,4 +218,77 @@ public class MainActivity extends AppCompatActivity {
             playerNameList.add(playerList.get(i).getPlayerName());
         }
     }
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
 }
